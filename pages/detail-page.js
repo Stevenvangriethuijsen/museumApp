e13d7d957b05862a003459971afbd444b54f4412
Stevenvangriethuijsen <Stevenvangriethuijsen@Outlook.com>
2020-01-31 12:23:04 +0100

    Store info input fields

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,3 +1,10 @@
 function submitComment() {
-  console.log("Hello!");
+  // get name and store it
+  const inputField = document.getElementById("name");
+  const name = inputField.value;
+  console.log(name);
+  // get message of user and store it
+  const textArea = document.getElementById("msg");
+  const msg = textArea.value;
+  console.log(msg);
 }
